4ec67732991230b6b20fd2f535204be3d6db16ef
Josews <jjosews@gmail.com>
2022-07-02 16:23:33 +0500

    added team section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,15 @@
 $(function(){
 
+  $('.team__items').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev"><svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M33.75 40.5L20.25 27L33.75 13.5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.25 40.5L33.75 27L20.25 13.5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+    draggable: false,
+    fade: true,
+    dots: true
+  });
+
   $('.portfolio__items').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
