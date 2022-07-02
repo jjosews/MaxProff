4ba758e5b41dacbdb740b4b268b8f117feb445b4
Josews <jjosews@gmail.com>
2022-07-02 15:50:05 +0500

    added reviews section

--- gulpfile.js
+++ gulpfile.js
@@ -36,6 +36,7 @@ function scripts() {
     'node_modules/jquery/dist/jquery.js',
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
